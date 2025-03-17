4e85b3677d782d6e04fcb34e1da269d3f7dffc3c
Mell0r <malovtarasov@gmail.com>
2025-03-17 12:52:46 +0300

    feat: added 2 first practices

--- practice1/main.cpp
+++ practice1/main.cpp
@@ -2,7 +2,9 @@
 #include <SDL.h>
 #undef main
 #else
+
 #include <SDL2/SDL.h>
+
 #endif
 
 #include <GL/glew.h>
@@ -11,31 +13,100 @@
 #include <stdexcept>
 #include <iostream>
 
-std::string to_string(std::string_view str)
-{
+std::string to_string(std::string_view str) {
     return std::string(str.begin(), str.end());
 }
 
-void sdl2_fail(std::string_view message)
-{
+void sdl2_fail(std::string_view message) {
     throw std::runtime_error(to_string(message) + SDL_GetError());
 }
 
-void glew_fail(std::string_view message, GLenum error)
-{
+void glew_fail(std::string_view message, GLenum error) {
     throw std::runtime_error(to_string(message) + reinterpret_cast<const char *>(glewGetErrorString(error)));
 }
 
-int main() try
+struct graphics_error : public std::runtime_error {
+    explicit graphics_error(const char *error) :
+            runtime_error(error) {}
+};
+
+struct shader_compilation_error : public graphics_error {
+    explicit shader_compilation_error(const char *error) :
+            graphics_error(error) {}
+};
+
+struct program_link_error : public graphics_error {
+    explicit program_link_error(const char *error) :
+            graphics_error(error) {}
+};
+
+GLuint create_shader(GLenum shader_type, const char *shader_source) {
+    GLuint shader = glCreateShader(shader_type);
+    glShaderSource(shader, 1, &shader_source, nullptr);
+    glCompileShader(shader);
+    GLint vertex_compiled;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &vertex_compiled);
+    if (vertex_compiled == GL_FALSE) {
+        GLint log_length;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_length);
+        std::string log(log_length, '\0');
+        glGetShaderInfoLog(shader, log_length, &log_length, log.data());
+        throw shader_compilation_error(log.data());
+    } else return shader;
+}
+
+GLuint create_program(GLuint vertex_shader, GLuint fragment_shader) {
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+    GLint program_linked;
+    glGetProgramiv(program, GL_LINK_STATUS, &program_linked);
+    if (program_linked == GL_FALSE) {
+        GLint log_length;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &log_length);
+        std::string log(log_length, '\0');
+        glGetProgramInfoLog(program, log_length, &log_length, log.data());
+        throw program_link_error(log.data());
+    } else return program;
+}
+
+const char fragment_source[] = R"(#version 330 core
+layout (location = 0) out vec4 out_color;
+
+in vec2 coord;
+void main()
 {
+    // vec4(R, G, B, A)
+    int col = (int(coord[0] * 10) + int(coord[1] * 10)) % 2;
+    out_color = vec4(float(col), float(col), float(col), 1.0);
+}
+)";
+
+const char vertex_source[] = R"(#version 330 core
+const vec2 VERTICES[3] = vec2[3](
+    vec2(0.0, 0.0),
+    vec2(1.0, 0.0),
+    vec2(0.0, 1.0)
+);
+
+out vec2 coord;
+void main()
+{
+    gl_Position = vec4(VERTICES[gl_VertexID], 0.0, 1.0);
+    coord = vec2(gl_Position);
+}
+)";
+
+int main() try {
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
         sdl2_fail("SDL_Init: ");
 
-    SDL_Window * window = SDL_CreateWindow("Graphics course practice 1",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        800, 600,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED);
+    SDL_Window *window = SDL_CreateWindow("Graphics course practice 1",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          800, 600,
+                                          SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED);
 
     if (!window)
         sdl2_fail("SDL_CreateWindow: ");
@@ -57,20 +128,36 @@ int main() try
 
     glClearColor(0.8f, 0.8f, 1.f, 0.f);
 
+    const char *str = "abc";
+    try {
+        create_shader(GL_FRAGMENT_SHADER, str);
+    } catch (shader_compilation_error &e) {
+        std::cout << e.what();
+    }
+
+    const GLuint vertex_shader = create_shader(GL_VERTEX_SHADER, vertex_source);
+    const GLuint fragment_shader = create_shader(GL_FRAGMENT_SHADER, fragment_source);
+    const GLuint program = create_program(vertex_shader, fragment_shader);
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+
     bool running = true;
-    while (running)
-    {
-        for (SDL_Event event; SDL_PollEvent(&event);) switch (event.type)
-        {
-        case SDL_QUIT:
-            running = false;
-            break;
-        }
+    while (running) {
+        for (SDL_Event event; SDL_PollEvent(&event);)
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = false;
+                    break;
+            }
 
         if (!running)
             break;
 
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         SDL_GL_SwapWindow(window);
     }
@@ -78,8 +165,7 @@ int main() try
     SDL_GL_DeleteContext(gl_context);
     SDL_DestroyWindow(window);
 }
-catch (std::exception const & e)
-{
+catch (std::exception const &e) {
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
 }
--- practice2/main.cpp
+++ practice2/main.cpp
@@ -2,7 +2,9 @@
 #include <SDL.h>
 #undef main
 #else
+
 #include <SDL2/SDL.h>
+
 #endif
 
 #include <GL/glew.h>
@@ -13,23 +15,20 @@
 #include <chrono>
 #include <unordered_map>
 
-std::string to_string(std::string_view str)
-{
+std::string to_string(std::string_view str) {
     return std::string(str.begin(), str.end());
 }
 
-void sdl2_fail(std::string_view message)
-{
+void sdl2_fail(std::string_view message) {
     throw std::runtime_error(to_string(message) + SDL_GetError());
 }
 
-void glew_fail(std::string_view message, GLenum error)
-{
+void glew_fail(std::string_view message, GLenum error) {
     throw std::runtime_error(to_string(message) + reinterpret_cast<const char *>(glewGetErrorString(error)));
 }
 
 const char vertex_shader_source[] =
-R"(#version 330 core
+        R"(#version 330 core
 
 const vec2 VERTICES[3] = vec2[3](
     vec2(0.0, 1.0),
@@ -43,18 +42,21 @@ const vec3 COLORS[3] = vec3[3](
     vec3(0.0, 0.0, 1.0)
 );
 
+uniform float scale;
+uniform mat4 transform;
+uniform mat4 view;
 out vec3 color;
 
 void main()
 {
     vec2 position = VERTICES[gl_VertexID];
-    gl_Position = vec4(position, 0.0, 1.0);
+    gl_Position = view * transform * vec4(position, 0.0, 1.0);
     color = COLORS[gl_VertexID];
 }
 )";
 
 const char fragment_shader_source[] =
-R"(#version 330 core
+        R"(#version 330 core
 
 in vec3 color;
 
@@ -66,15 +68,13 @@ void main()
 }
 )";
 
-GLuint create_shader(GLenum type, const char * source)
-{
+GLuint create_shader(GLenum type, const char *source) {
     GLuint result = glCreateShader(type);
     glShaderSource(result, 1, &source, nullptr);
     glCompileShader(result);
     GLint status;
     glGetShaderiv(result, GL_COMPILE_STATUS, &status);
-    if (status != GL_TRUE)
-    {
+    if (status != GL_TRUE) {
         GLint info_log_length;
         glGetShaderiv(result, GL_INFO_LOG_LENGTH, &info_log_length);
         std::string info_log(info_log_length, '\0');
@@ -84,8 +84,7 @@ GLuint create_shader(GLenum type, const char * source)
     return result;
 }
 
-GLuint create_program(GLuint vertex_shader, GLuint fragment_shader)
-{
+GLuint create_program(GLuint vertex_shader, GLuint fragment_shader) {
     GLuint result = glCreateProgram();
     glAttachShader(result, vertex_shader);
     glAttachShader(result, fragment_shader);
@@ -93,8 +92,7 @@ GLuint create_program(GLuint vertex_shader, GLuint fragment_shader)
 
     GLint status;
     glGetProgramiv(result, GL_LINK_STATUS, &status);
-    if (status != GL_TRUE)
-    {
+    if (status != GL_TRUE) {
         GLint info_log_length;
         glGetProgramiv(result, GL_INFO_LOG_LENGTH, &info_log_length);
         std::string info_log(info_log_length, '\0');
@@ -105,16 +103,15 @@ GLuint create_program(GLuint vertex_shader, GLuint fragment_shader)
     return result;
 }
 
-int main() try
-{
+int main() try {
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
         sdl2_fail("SDL_Init: ");
 
-    SDL_Window * window = SDL_CreateWindow("Graphics course practice 2",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        800, 600,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED);
+    SDL_Window *window = SDL_CreateWindow("Graphics course practice 2",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          800, 600,
+                                          SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_MAXIMIZED);
 
     if (!window)
         sdl2_fail("SDL_CreateWindow: ");
@@ -128,6 +125,8 @@ int main() try
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+    SDL_GL_SetSwapInterval(0);
+
     if (!gl_context)
         sdl2_fail("SDL_GL_CreateContext: ");
 
@@ -143,6 +142,11 @@ int main() try
     GLuint fragment_shader = create_shader(GL_FRAGMENT_SHADER, fragment_shader_source);
 
     GLuint program = create_program(vertex_shader, fragment_shader);
+    glUseProgram(program);
+    GLint scaleUniformVar = glGetUniformLocation(program, "scale");
+    glUniform1f(scaleUniformVar, 2.0);
+    GLint transformUniformVar = glGetUniformLocation(program, "transform");
+    GLint viewUniformVar = glGetUniformLocation(program, "view");
 
     GLuint vao;
     glGenVertexArrays(1, &vao);
@@ -151,52 +155,79 @@ int main() try
 
     auto last_frame_start = std::chrono::high_resolution_clock::now();
 
+    float time = 0.f;
+
     bool running = true;
-    while (running)
-    {
-        for (SDL_Event event; SDL_PollEvent(&event);) switch (event.type)
-        {
-        case SDL_QUIT:
-            running = false;
-            break;
-        case SDL_WINDOWEVENT: switch (event.window.event)
-            {
-            case SDL_WINDOWEVENT_RESIZED:
-                width = event.window.data1;
-                height = event.window.data2;
-                glViewport(0, 0, width, height);
-                break;
+    while (running) {
+        for (SDL_Event event; SDL_PollEvent(&event);)
+            switch (event.type) {
+                case SDL_QUIT:
+                    running = false;
+                    break;
+                case SDL_WINDOWEVENT:
+                    switch (event.window.event) {
+                        case SDL_WINDOWEVENT_RESIZED:
+                            width = event.window.data1;
+                            height = event.window.data2;
+                            glViewport(0, 0, width, height);
+                            break;
+                    }
+                    break;
+                case SDL_KEYDOWN:
+                    key_down[event.key.keysym.sym] = true;
+                    break;
+                case SDL_KEYUP:
+                    key_down[event.key.keysym.sym] = false;
+                    break;
             }
-            break;
-        case SDL_KEYDOWN:
-            key_down[event.key.keysym.sym] = true;
-            break;
-        case SDL_KEYUP:
-            key_down[event.key.keysym.sym] = false;
-            break;
-        }
 
         if (!running)
             break;
 
         auto now = std::chrono::high_resolution_clock::now();
-        float dt = std::chrono::duration_cast<std::chrono::duration<float>>(now - last_frame_start).count();
+        float dt = 0.016f;
+        std::cout << "Actual dt: "
+                  << std::chrono::duration_cast<std::chrono::duration<float>>(now - last_frame_start).count()
+                  << std::endl;
         last_frame_start = now;
 
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(program);
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        float angle = time;
+        int timeInt = (int) time / 10;
+        float shift = timeInt % 2 - 1 + (time / 10 - timeInt);
+        float x = timeInt % 4 < 2 ? shift : -shift;
+        float y = x;
+        float transform[16] = {
+                cos(angle) / 2, -sin(angle) / 2, 0, x,
+                sin(angle) / 2, cos(angle) / 2, 0, y,
+                0, 0, 1, 0,
+                0, 0, 0, 1
+        };
+        float aspect_ratio = (float) width / height;
+        float view[16] = {
+                1 / aspect_ratio, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, 1
+        };
+
+        glUniformMatrix4fv(transformUniformVar, 1, GL_TRUE, transform);
+        glUniformMatrix4fv(viewUniformVar, 1, GL_TRUE, view);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 9);
 
         SDL_GL_SwapWindow(window);
+
+        time += dt;
     }
 
     SDL_GL_DeleteContext(gl_context);
     SDL_DestroyWindow(window);
 }
-catch (std::exception const & e)
-{
+catch (std::exception const &e) {
     std::cerr << e.what() << std::endl;
     return EXIT_FAILURE;
 }
